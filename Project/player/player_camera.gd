40f10df5a53dc10344dee54f0bc6a6adab6240fe
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2024-03-29 14:29:53 -0500

    Update .gitignore

--- Project/player/player_camera.gd
+++
@@ -1,6 +0,0 @@
-extends Camera2D
-
-@onready var camera_center = $camera_center
-
-func _process(_delta):
-	camera_center.global_position = get_screen_center_position()
--- start_editor.bat
+++
@@ -1,1 +0,0 @@
-..\Godot_v4.2.1-stable_win64.exe project/project.godot
